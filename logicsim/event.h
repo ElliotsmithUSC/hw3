3fa38f52b6461f7274c179c80a8f0aca1f0c2495
Elliot Smith <elliotas@usc.edu>
2022-10-18 06:37:13 +0000

    All done

--- logicsim/event.h
+++ logicsim/event.h
@@ -10,7 +10,16 @@ struct Event
 };
 
 typedef struct EventLess {
+    bool operator() (Event* event1, Event* event2){
+        if(event1->time < event2->time){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
         //write the operator() required to make this a functor that compares Events by time
 } EventLess;
 	
+
 #endif
