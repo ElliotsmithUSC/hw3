9cc3c9b0c2b98e76d5744d0e3e226560b44f8ceb
Elliot Smith <elliotas@usc.edu>
2022-10-18 06:22:30 +0000

    All done

--- heap.h
+++ heap.h
@@ -2,6 +2,7 @@
 #define HEAP_H
 #include <functional>
 #include <stdexcept>
+#include <vector>
 
 template <typename T, typename PComparator = std::less<T> >
 class Heap
@@ -61,34 +62,78 @@ public:
 
 private:
   /// Add whatever helper functions and data members you need below
-
-
-
+  std::vector<T> nodes;
+  int m;
+  PComparator c;
+  void trickleUp(size_t location);
+  void heapify(size_t location);
 
 };
 
 // Add implementation of member functions here
+template <typename T, typename PComparator>
+Heap<T,PComparator>::Heap(int m, PComparator c){
+  this->m = m;
+  this->c = c;
+}
 
+template<typename T, typename PComparator>
+Heap<T,PComparator>::~Heap(){}
+
+template<typename T, typename PComparator>
+void Heap<T,PComparator>::trickleUp(size_t location){
+  int parentnode = location/2;
+  while(c(nodes[location],nodes[parentnode])){
+    T temp = nodes[parentnode];
+    nodes[parentnode] = nodes[location];
+    nodes[location] = temp;
+		location = parentnode;
+		parentnode = parentnode/2;
+  }
+}
+
+template<typename T, typename PComparator>
+void Heap<T,PComparator>::heapify(size_t location){
+	if(nodes.empty()){
+		return;
+	}
+	if(nodes.size()-1 < location*2){
+		return;
+	}
+	int betterChild = location*2;
+	if(betterChild == 0){
+		betterChild = 1;
+	}
+	if(nodes.size()-1 > (location*2) + 1){
+		int rChild = betterChild++;
+		if(c(nodes[rChild], nodes[betterChild])){
+			betterChild = rChild;
+		}
+	}
+	if(c(nodes[betterChild],nodes[location])){
+		T temp = nodes[location];
+		nodes[location] = nodes[betterChild];
+		nodes[betterChild] = temp;
+		heapify(betterChild);
+	}
+}
+
+template<typename T, typename PComparator>
+void Heap<T,PComparator>::push(const T& item){
+	nodes.push_back(item);
+	int pushlocation =  nodes.size() - 1;
+	trickleUp(pushlocation);
+}
 
 // We will start top() for you to handle the case of 
 // calling top on an empty heap
 template <typename T, typename PComparator>
 T const & Heap<T,PComparator>::top() const
 {
-  // Here we use exceptions to handle the case of trying
-  // to access the top element of an empty heap
   if(empty()){
-    // ================================
-    // throw the appropriate exception
-    // ================================
-
-
+    throw std::logic_error("the heap is empty");
   }
-  // If we get here we know the heap has at least 1 item
-  // Add code to return the top element
-
-
-
+  return nodes[0];
 }
 
 
@@ -98,15 +143,26 @@ template <typename T, typename PComparator>
 void Heap<T,PComparator>::pop()
 {
   if(empty()){
-    // ================================
-    // throw the appropriate exception
-    // ================================
-
-
+    throw std::logic_error("the heap is empty");
   }
+	nodes[0] = nodes.back();
+	nodes.pop_back();
+	heapify(0);
+}
 
+template <typename T, typename PComparator>
+bool Heap<T,PComparator>::empty() const{
+	if(nodes.size()==0){
+		return true;
+	}
+	else{
+		 return false;
+	}
+}
 
-
+template <typename T, typename PComparator>
+size_t Heap<T,PComparator>::size() const{
+	return nodes.size();
 }
 
 
